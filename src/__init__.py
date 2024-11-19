770b970e6fed9906ab69d0a5ee549343805c4f5c
MoriguchiHinata <forest.tp03@gmail.com>
2024-11-19 23:39:28 +0900

    フォルダ構成の変更

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+# This is the __init__.py file for the just-intonation-visualizer package.
+# It can be used to initialize the package and import necessary modules.
+
+# Example of importing a module within the package
+# from .module_name import ClassName
+
+# You can also define package-level variables or functions here if needed.
+
+__version__ = '0.1.0'
