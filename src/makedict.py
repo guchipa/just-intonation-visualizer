e36f0f8d19ea43b2589ae0c989bbf0628cb9b9d6
MoriguchiHinata <forest.tp03@gmail.com>
2024-12-18 23:12:39 +0900

    音名辞書，リストの定数化

--- src/makedict.py
+++ src/makedict.py
@@ -1,25 +1,16 @@
-name_dict = {
-    1: "C",
-    2: "C#(D♭)",
-    3: "D",
-    4: "D#(E♭)",
-    5: "E",
-    6: "F",
-    7: "F#(G♭)",
-    8: "G",
-    9: "G#(A♭)",
-    10: "A",
-    11: "A#(B♭)",
-    12: "B",
-}
+import json
+
+with open("../config/constants.json", "r") as f:
+    constants = json.load(f)
+
+name_dict = constants["name_dict"]
+A4_FREQ = constants["A4_FREQ"]
 
 f = open("./dict.txt", "w")
-
-A4_FREQ = 440
 
 for i in range(1, 7):
     for j in range(1, 13):
         base = A4_FREQ * (2 ** (i - 4))
-        f.write(f'"{name_dict[j]}{i}": {base * (2 ** ((j - 10) / 12))},\n')
+        f.write(f'"{name_dict[str(j)]}{i}": {base * (2 ** ((j - 10) / 12))},\n')
 
 f.close()
